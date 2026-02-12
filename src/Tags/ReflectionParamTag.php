bd29364d55c0430821de907d035b684b635dca5e
AMJones <am@jonesiscoding.com>
2026-02-12 07:39:52 -0600

    ParamTag type as Type

--- src/Tags/ReflectionParamTag.php
+++ src/Tags/ReflectionParamTag.php
@@ -2,6 +2,8 @@
 
 namespace DevCoding\Reflection\Tags;
 
+use DevCoding\Reflection\Types\Type;
+
 /**
  * @author  AMJones <am@jonesiscoding.com>
  * @license https://github.com/jonesiscoding/php-reflection/blob/main/LICENSE
@@ -11,11 +13,15 @@ class ReflectionParamTag extends ReflectionTag implements NamedTagInterface
   public function __construct(\ReflectionMethod $method, string $contents)
   {
     $property = ['tag' => 'param'];
-    if (preg_match(self::STANDARD, $contents, $tag))
+    if (preg_match(self::STANDARD, '@param '.$contents, $tag))
     {
       $property['name']        = $tag['name'];
-      $property['type']        = $tag['type'];
-      $property['description'] = $tag['description'];
+      $property['description'] = $tag['description'] ?? null;
+
+      if (!empty($tag['type']))
+      {
+        $property['type'] = Type::from($tag['type'], $method);
+      }
     }
 
     parent::__construct($method, array_filter($property));
