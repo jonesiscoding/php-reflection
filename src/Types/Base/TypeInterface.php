fed9b78d960bc41767b169e258c872628bd02e7f
AMJones <am@jonesiscoding.com>
2026-02-14 12:27:41 -0600

    Adds PHPdocs

--- src/Types/Base/TypeInterface.php
+++ src/Types/Base/TypeInterface.php
@@ -4,9 +4,19 @@ namespace DevCoding\Reflection\Types\Base;
 
 use DevCoding\Reflection\Types\Factory\Match;
 
+/**
+ * Interface for object classes describing a PHP type
+ *
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @author  AMJones <am@jonesiscoding.com>
+ * @license https://github.com/jonesiscoding/php-reflection/blob/main/LICENSE
+ */
 interface TypeInterface
 {
   /**
+   * MUST evaluate if the given string and Reflector can be described by this type.
+   * MUST populate the given $matches argument with applicable match data
+   *
    * @param string          $string
    * @param \Reflector|null $context
    * @param array{ type: string, shape: string } $matches
